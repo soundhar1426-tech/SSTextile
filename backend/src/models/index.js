760c200bbeda7592b163e54bf18d1f68398844df
soundhar1426-tech <soundhar1426@gmail.com>
2026-09-21 14:43:01 +0530

    Push frontend only

--- backend/src/models/index.js
+++
@@ -1,7 +0,0 @@
-export { User } from './User.js';
-export { Product } from './Product.js';
-export { Size } from './Size.js';
-export { Order } from './Order.js';
-export { OrderItem } from './OrderItem.js';
-export { Invoice } from './Invoice.js';
-export { Settings } from './Settings.js';
